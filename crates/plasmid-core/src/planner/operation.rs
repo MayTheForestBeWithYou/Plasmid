c99fc1dfd3d2e09efde6c26b47880a9e31d25c40
MayTheForestBeWithYou <timonvl@outlook.com>
2026-03-07 17:34:25 +0100

    feat: Print paths with consistent OS formatting

--- crates/plasmid-core/src/planner/operation.rs
+++ crates/plasmid-core/src/planner/operation.rs
@@ -1,6 +1,6 @@
 use colored::Colorize;
 use plasmid_fs::filesystem::operation::FileSystemOps;
-use std::path::PathBuf;
+use std::path::{MAIN_SEPARATOR, Path, PathBuf};
 
 use crate::planner::error::PlannerError;
 
@@ -14,14 +14,18 @@ impl Operation {
     pub fn pretty_print(&self) {
         match self {
             Self::MkDir { path } => {
-                println!("  {} {}", "+ DIR ".green().bold(), path.display());
+                println!(
+                    "  {} {}",
+                    "+ DIR ".green().bold(),
+                    Self::normalize_path_to_os(path)
+                );
             }
             Self::Link { src, dest } => {
                 println!(
                     "  {} {} -> {}",
                     "+ LINK".cyan().bold(),
-                    src.display(),
-                    dest.display()
+                    Self::normalize_path_to_os(src),
+                    Self::normalize_path_to_os(dest)
                 );
             }
         }
@@ -44,5 +48,17 @@ impl Operation {
             }
         }
         Ok(())
+    }
+
+    fn normalize_path_to_os(path: &Path) -> String {
+        let mut out = String::new();
+        for (i, comp) in path.components().enumerate() {
+            if i > 0 {
+                out.push(MAIN_SEPARATOR);
+            }
+            out.push_str(&comp.as_os_str().to_string_lossy());
+        }
+
+        out
     }
 }
